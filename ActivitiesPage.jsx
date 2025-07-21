ad78457606500a1c018f5ce86559d028e50fda88
EmilieSJKL612 <151094902+EmilieSJKL612@users.noreply.github.com>
2025-07-21 04:38:42 -0600

    Add files via upload

---
+++ ActivitiesPage.jsx
@@ -0,0 +1,15 @@
+
+import { useState } from "react";
+import { useAuth } from "../auth/AuthContext";
+import useMutation from "../api/useMutation";
+import useQuery from "../api/useQuery";
+
+
+export default function ActivitiesPage() {
+  return (
+    <>
+      <h1>Activities</h1>
+      <p>Imagine all the activities!</p>
+    </>
+  );
+}
